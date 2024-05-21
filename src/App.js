bb178a649d22e7aad6c03679003a06a76e9aaae8
istafamarshall <istafa.a.marshall@gmail.com>
2024-05-21 17:27:54 -0400

    update post file with names

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Post from './components/Post';
+
 
 function App() {
   return (
     <div className="App">
-      <h1> My React App</h1>
+      
+            
+      <Post />
     </div>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,12 @@
+const names = ['Istafa' , 'Anthony']
+
 function Post() {
+
+    const name = Math.random() > 0.5 ? names[0] : names [1];
+
     return (
         <div>
-            <h2>Post Title</h2>
+            <h2>{name}</h2>
             <p>Post Content</p>
         </div>
     );
