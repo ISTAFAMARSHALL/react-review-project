e0eae947b78258627835dc3f6df0cfab05ebbb97
istafamarshall <istafa.a.marshall@gmail.com>
2024-06-03 18:49:16 -0400

    update New Post

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,22 +1,48 @@
 import classes from './NewPost.module.css'
+import { useState } from 'react'
 
 function NewPost (props) {
 
+    const [ enteredBody, setEnteredBody ] = useState('');
+    const [ enteredName, setEnteredName ] = useState('');
+    
+
+    function bodyChangeHandler (event) {
+        setEnteredBody(event.target.value)
+    }
+
+    function nameChangeHandler (event) {
+        setEnteredName(event.target.value)
+    }
+
+    function submitHandler (event) {
+        event.preventDefault();
+        
+        const postData = {
+            body: enteredBody,
+            name: enteredName
+        };
+        
+        props.onCancel()
+    }
+
 
     return (
-        <form className={classes.form} >
+        <form className={classes.form} onSubmit={submitHandler} >
             <p>
                 <label htmlFor='body'>Text</label>
-                <textarea id='body' required rows={3} onChange={props.onBodyChange} />
+                <textarea id='body' required rows={3} onChange={bodyChangeHandler} />
             </p>
 
             <p>
                 <label htmlFor='name'>Your Name</label>
-                <input type='text' id='name' required onChange={props.onNameChange} />
+                <input type='text' id='name' required onChange={nameChangeHandler} />
             </p>
 
             <p className={classes.action} >
+                
                 <button type="button" onClick={props.onCancel}>Cancel</button>
+                
                 <button>Submit</button>
             </p>
         </form>
--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import Posts from './Posts'
 import classes from './PostsList.module.css'
 import NewPost from './NewPost'
@@ -6,31 +5,17 @@ import Modal from './Modal'
 
 function PostsList (props) {
 
-    const [ enteredBody, setEnteredBody ] = useState('');
-    const [ enteredName, setEnteredName ] = useState('');
-    
-
-
-    function bodyChangeHandler (event) {
-        setEnteredBody(event.target.value)
-    }
-
-    function nameChangeHandler (event) {
-        setEnteredName(event.target.value)
-    }
-
-
     return (
 
         <>
 
             {props.isPosting && (<Modal onclose={props.onStopingPost} >
-                <NewPost onBodyChange={bodyChangeHandler} onNameChange={nameChangeHandler} onCancel={props.onStopingPost} />
+                <NewPost onCancel={props.onStopingPost} />
             </Modal>) }
 
             <ul className={classes.posts}>
 
-                <Posts name={enteredName} body={enteredBody}/>
+                {/* <Posts name={enteredName} body={enteredBody}/> */}
                 <Posts name="Quran" body="Hello I am Quran!"/>
                 <Posts name="Jackie" body="Hello I am Jackie!"/>
             
