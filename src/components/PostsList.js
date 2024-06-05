ff1289fd2d78ecc5ef7240356f7cb6ebc83fbe77
istafamarshall <istafa.a.marshall@gmail.com>
2024-06-05 01:36:43 -0400

    update Post list with fetch request

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -9,12 +9,20 @@ function PostsList (props) {
     const [ posts , setPosts] = useState([]);
 
     function addPostHandler(postData) {
+
+        fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        
         setPosts((existingPost) => [postData, ...existingPost])
     }
 
-    console.log(posts)
     let displayPosts = posts.map((e) => (
-        <Posts name={e.name} body={e.body} />
+        <Posts key={e.id} name={e.name} body={e.body} />
     ))
 
     return (
@@ -27,10 +35,11 @@ function PostsList (props) {
 
             <ul className={classes.posts}>
                 
-                {displayPosts}
-                {/* <Posts name={enteredName} body={enteredBody}/> */}
-                {/* <Posts name="Quran" body="Hello I am Quran!"/>
-                <Posts name="Jackie" body="Hello I am Jackie!"/> */}
+                { posts.length > 0 ? displayPosts : (
+                    <div style={{ textAlign: 'center' , color: 'red' }} > 
+                        <h2 >There are  no posts yet</h2> 
+                        <p>Create Some!</p>
+                    </div>) } 
             
             </ul>
 
